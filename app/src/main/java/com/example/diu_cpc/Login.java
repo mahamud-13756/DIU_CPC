14b21abe6bee53bb92b153eb5962ae14773e69c9
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-05-21 23:20:52 +0600

    design

--- app/src/main/java/com/example/diu_cpc/Login.java
+++ app/src/main/java/com/example/diu_cpc/Login.java
@@ -3,12 +3,11 @@ package com.example.diu_cpc;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Option extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_option);
-
+        setContentView(R.layout.activity_login);
     }
 }
